2a65fbf5616590951ed6365edb930ce623303c75
imuslic1 <ismar.muslic@outlook.com>
2026-04-08 22:13:51 +0200

    Project setup

---
+++ src/main/java/com/catering/analyticsadmin/AnalyticsAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.catering.analyticsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsAdminServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catering/analyticsadmin/AnalyticsAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catering.analyticsadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
